bdf3ccff2247afb1bc135b4bae6b9941aa35d37e
SOHEE JANG <jsh167528@gmail.com>
2019-07-15 23:43:08 +0900

    MODIFY: remove needless code

--- haagen-dazs/src/App.tsx
+++ haagen-dazs/src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Switch, BrowserRouter, Route } from "react-router-dom";
 import { hot } from "react-hot-loader/root";
+
 import { GlobalStyle } from "../public/GlobalStyle";
 import { LoginPage } from "./pages";
 
