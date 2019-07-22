10cc24c18dbc745ed895778feaba33a78bb02cb3
SOHEE JANG <jsh167528@gmail.com>
2019-07-22 14:14:14 +0900

    CHANGE: code review

--- haagen-dazs/src/components/AdminCertify/index.tsx
+++ haagen-dazs/src/components/AdminCertify/index.tsx
@@ -9,41 +9,54 @@ export interface State {
   inputID: string;
   inputPW: string;
   isLogin: boolean;
+  isFail: boolean;
 }
 
-class AdminCetify extends React.Component<State> {
+class AdminCetify extends React.Component<any, State> {
   state: State = {
     inputID: "",
     inputPW: "",
-    isLogin: false
+    isLogin: false,
+    isFail: false
   };
 
-  componentDidMount = () => {
-    this.setState(() => {
-      if (sessionStorage.getItem("accessTooken")) {
-        return { isLogin: true };
-      } else {
-        return { isLogin: false };
-      }
-    });
+  componentDidMount = (): void =>
+    this.setState(() => this.getAccessToeknFromStorage());
+
+  private getAccessToeknFromStorage = (): { isLogin: boolean } => {
+    if (
+      sessionStorage.getItem("accessToken") &&
+      sessionStorage.getItem("accessToken") !== null
+    )
+      return { isLogin: true };
+    else return { isLogin: false };
   };
 
-  private inputAuthInfo = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
+  private handleInputId = (e: React.ChangeEvent<HTMLInputElement>): void =>
+    this.setState({ inputID: e.target.value });
+
+  private handleInputPw = (e: React.ChangeEvent<HTMLInputElement>): void =>
+    this.setState({ inputPW: e.target.value });
 
   private authInfoSubmit = (): void => {
-    if (this.state.inputID !== "" && this.state.inputPW !== "") {
+    if (this.state.inputID && this.state.inputPW) {
       getUserToken({
         email: this.state.inputID,
         password: this.state.inputPW
       })
         .then(response => {
           localStorage.setItem("refreshToken", response.data.refreshToken);
-          sessionStorage.setItem("accessToken", response.data.accessToken);
+          // sessionStorage.setItem("accessToken", response.data.accessToken);
+          sessionStorage.setItem("accessToken", response.data.access_token);
           this.setState({ isLogin: true });
         })
-        .catch(error => console.log(error));
+        .catch(error => {
+          if (error.response.status === 403) {
+            this.setState({ isFail: true });
+          } else {
+            console.log(error.response);
+          }
+        });
     }
   };
 
@@ -57,19 +70,28 @@ class AdminCetify extends React.Component<State> {
         <S.CertifyDescription>
           지급받은 아이디와 비밀번호를 입력해주세요
         </S.CertifyDescription>
-        <S.CertifyInput
-          name="inputID"
-          placeholder="ID"
-          value={this.state.inputID}
-          onChange={this.inputAuthInfo}
-        />
-        <S.CertifyInput
-          name="inputPW"
-          placeholder="Password"
-          type="password"
-          value={this.state.inputPW}
-          onChange={this.inputAuthInfo}
-        />
+        <S.CertifyInputWrapper>
+          <S.CertifyInput
+            name="inputID"
+            placeholder="ID"
+            value={this.state.inputID}
+            onChange={this.handleInputId}
+          />
+          <S.ErrorMessage isFail={this.state.isFail}>
+            아이디 혹은 비밀번호가 일치하지 않습니다.
+          </S.ErrorMessage>
+        </S.CertifyInputWrapper>
+
+        <S.CertifyInputWrapper>
+          <S.CertifyInput
+            name="inputPW"
+            placeholder="Password"
+            type="password"
+            value={this.state.inputPW}
+            onChange={this.handleInputPw}
+          />
+        </S.CertifyInputWrapper>
+
         <S.CertifyBtn
           onClick={this.authInfoSubmit}
           isActivation={
