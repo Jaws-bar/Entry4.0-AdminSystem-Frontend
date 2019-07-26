0d4bae92ee76f229bfe246bd9c56949af22382de
SOHEE JANG <jsh167528@gmail.com>
2019-07-26 14:06:23 +0900

    CREATE: header-component publishing

---
+++ haagen-dazs/src/utils/Header/index.tsx
@@ -0,0 +1,43 @@
+import * as React from "react";
+import { Link } from "react-router-dom";
+
+import * as S from "./style";
+import Logo from "../../assets/header-component/EntryDSM_LOGO.png";
+import SearchIconImg from "../../assets/header-component/ic_search.png";
+
+const Header: React.FC = () => {
+  const clearStorage = (): void => {
+    sessionStorage.clear();
+    localStorage.clear();
+  };
+  return (
+    <S.HeaderWrapper>
+      <S.ContentWrapper>
+        <Link to="/">
+          <S.EntryLogo src={Logo} alt="" />
+        </Link>
+        <S.SearchInputWrapper isActivation={true}>
+          <S.SearchIcon src={SearchIconImg} alt="" />
+          <S.SearchInput placeholder="검색어를 입력해주세요." />
+        </S.SearchInputWrapper>
+        <S.MenuList>
+          <S.MenuListItem>
+            <Link to="/">Q{"&"}A</Link>
+            <S.UnderLine isActivation={true} />
+          </S.MenuListItem>
+          <S.MenuListItem>
+            <Link to="/">통계</Link>
+            <S.UnderLine isActivation={true} />
+          </S.MenuListItem>
+          <S.MenuListItem>
+            <Link to="/administration">지원자 목록</Link>
+            <S.UnderLine isActivation={true} />
+          </S.MenuListItem>
+        </S.MenuList>
+
+        <S.LogoutBtn onClick={clearStorage}>로그아웃</S.LogoutBtn>
+      </S.ContentWrapper>
+    </S.HeaderWrapper>
+  );
+};
+export default Header;
