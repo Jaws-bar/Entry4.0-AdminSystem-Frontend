8b70808e8b32ff470a315da329b1c6a2dfda0833
SOHEE JANG <jsh167528@gmail.com>
2019-08-14 22:43:54 +0900

    MODIFY: sementic

--- haagen-dazs/src/components/CompetitonView/index.tsx
+++ haagen-dazs/src/components/CompetitonView/index.tsx
@@ -7,26 +7,26 @@ interface Props {
   selectedItem: string;
 }
 
-const CompetitionView: React.FC<Props> = props => (
+const CompetitionView: React.FC<Props> = ({ selectedItem }) => (
   <S.CompetitionViewContainer>
-    {props.selectedItem === "전체" ? (
-      <React.Fragment>
+    {selectedItem === "전체" ? (
+      <>
         <Table />
         <S.ExcelOutputBtn>Excel 다운로드</S.ExcelOutputBtn>
-      </React.Fragment>
+      </>
     ) : (
-      <React.Fragment>
+      <>
         <S.CategoryItemWrapper style={{ marginBottom: "36px", width: "245px" }}>
           <S.CategoryName>경쟁률</S.CategoryName>
           <S.CategoryContent>187 : 1</S.CategoryContent>
           <S.Underline />
         </S.CategoryItemWrapper>
         <S.CategoryItemWrapper style={{ width: "353px" }}>
-          <S.CategoryName>{props.selectedItem} 지원자 수</S.CategoryName>
+          <S.CategoryName>{selectedItem} 지원자 수</S.CategoryName>
           <S.CategoryContent>187 명</S.CategoryContent>
           <S.Underline />
         </S.CategoryItemWrapper>
-      </React.Fragment>
+      </>
     )}
   </S.CompetitionViewContainer>
 );
