b52452ad11a3bdb55c0d0a34898ce82121d4208c
SOHEE JANG <jsh167528@gmail.com>
2019-08-24 13:46:00 +0900

    MODIFY: split up components

---
+++ haagen-dazs/src/components/ApplicantInfoView/Unsubmitted/ThirdSchoolContact.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+import * as S from "./style";
+
+const ThirdSchoolContactTR: React.FC = () => (
+  <S.UnsubmittedInfoTR>
+    <S.UnsubmittedInfoTH>학교 전화번호</S.UnsubmittedInfoTH>
+    <S.UnsubmittedInfoTD>042-1234-5678</S.UnsubmittedInfoTD>
+  </S.UnsubmittedInfoTR>
+);
+
+export default ThirdSchoolContactTR;
