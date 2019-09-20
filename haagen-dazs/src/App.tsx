a8e5f8703944b8f12e214a286a667862279bdfba
sohi_5 <38098147+soheejang15@users.noreply.github.com>
2019-09-20 13:50:16 +0900

    MODIFY: token handling

--- haagen-dazs/src/App.tsx
+++ haagen-dazs/src/App.tsx
@@ -1,18 +1,30 @@
-import React from "react";
+import React, { FC, useState } from "react";
 import { Switch, BrowserRouter, Route } from "react-router-dom";
 import { hot } from "react-hot-loader/root";
 
 import { GlobalStyle } from "../public/GlobalStyle";
 import { LoginPage, Main, AdminPage } from "./pages";
+import CheckToken from "./components/CheckToken";
 
-const App = () => {
+const App: FC = () => {
+  const [token, setToken] = useState<{
+    accessToken: string;
+    sessionToken: string;
+  }>({ accessToken: "", sessionToken: "" });
+
   return (
     <BrowserRouter>
       <GlobalStyle />
       <Switch>
-        <Route path="/" exact component={Main} />
-        <Route path="/login" exact component={LoginPage} />
-        <Route path="/administration" exact component={AdminPage} />
+        <Route
+          path="/login"
+          exact
+          render={() => <LoginPage setToken={setToken} />}
+        />
+        <CheckToken token={token}>
+          <Route path="/" exact component={Main} />
+          <Route path="/administration" exact component={AdminPage} />
+        </CheckToken>
       </Switch>
     </BrowserRouter>
   );
