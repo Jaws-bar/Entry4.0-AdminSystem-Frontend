3e7a3737a4a750db2c22c1332f1d9a2580336d1d
sohi_5 <38098147+soheejang15@users.noreply.github.com>
2019-09-20 14:25:03 +0900

    ADD: refresh token api

--- haagen-dazs/src/lib/api/index.ts
+++ haagen-dazs/src/lib/api/index.ts
@@ -1,21 +1,41 @@
 import { connectionUrl } from "./endpoint";
 import axios from "axios";
 
-interface payloadType {
-  email: string;
+interface PayloadType {
+  admin_id: string;
   password: string;
 }
 
-interface tokenType {
-  // accessToken: string;
-  access_token: string;
-  refreshToken: string;
+interface TokenType {
+  access: string;
+  refresh: string;
 }
 
-export const getUserToken = async (body: payloadType) => {
-  const response = await axios.post<tokenType>(
+export const getUserToken = async (body: PayloadType) => {
+  const response = await axios.post<TokenType>(
     `${connectionUrl}/admin/login`,
     body
   );
+  return response.data;
+};
+
+export const refreshUserToken = async (payload: { refreshToken: string }) => {
+  const response = await axios.patch<{ access: string }>(
+    `${connectionUrl}/admin/refresh`,
+    {
+      headers: {
+        refresh: `Bearer ${payload.refreshToken}`
+      }
+    }
+  );
+
+  return response.data;
+};
+
+export const cancelSubmitApplicant = async (payload: { email: string }) => {
+  const response = await axios.patch(
+    `${connectionUrl}/student/${payload.email}/submit`
+  );
+
   return response.data;
 };
