bba3d23eb1e1a359877150c45c36ac1a91f6643c
sohi_5 <38098147+soheejang15@users.noreply.github.com>
2019-09-20 14:20:35 +0900

    CHANGE: applicant status edit function

--- haagen-dazs/src/components/ApplicantInfoView/Submitted/DocumentContainer/index.tsx
+++ haagen-dazs/src/components/ApplicantInfoView/Submitted/DocumentContainer/index.tsx
@@ -1,18 +1,40 @@
 import * as React from "react";
-import { Component } from "react";
 
 import DocumentContent from "./DocumentContent";
 import DocumentSelectBar from "./DocumentSelectBar";
 
-class DocumentContainer extends React.Component {
-  render() {
+interface State {
+  isIntroductionSelected: boolean;
+  isStudyPlanSelected: boolean;
+}
+
+class DocumentContainer extends React.Component<{}, State> {
+  public state: State = {
+    isIntroductionSelected: true,
+    isStudyPlanSelected: false
+  };
+
+  public render() {
     return (
       <>
-        <DocumentSelectBar />
+        <DocumentSelectBar
+          handleOnClickIntroduction={this.handleOnClickIntroduction}
+          handleOnClickStudyPlan={this.handleOnClickStudyPlan}
+          isIntroductionSelected={this.state.isIntroductionSelected}
+          isStudyPlanSelected={this.state.isStudyPlanSelected}
+        />
         <DocumentContent />
       </>
     );
   }
+
+  public handleOnClickIntroduction = () => {
+    this.setState({ isIntroductionSelected: true, isStudyPlanSelected: false });
+  };
+
+  public handleOnClickStudyPlan = () => {
+    this.setState({ isIntroductionSelected: false, isStudyPlanSelected: true });
+  };
 }
 
 export default DocumentContainer;
--- haagen-dazs/src/components/ApplicantInfoView/Submitted/EditButtonContainer/index.tsx
+++ haagen-dazs/src/components/ApplicantInfoView/Submitted/EditButtonContainer/index.tsx
@@ -1,21 +1,23 @@
 import * as React from "react";
-import { Component } from "react";
 
 import * as S from "./style";
 import Checked from "../../../../assets/admin-page/checked.png";
+import { cancelSubmitApplicant } from "../../../../lib/api";
 
 interface State {
   isApplicationArrived: boolean;
   isPaymentCompleted: boolean;
 }
 
-class EditButtonContainer extends React.Component<any, State> {
-  state: State = {
+class EditButtonContainer extends React.Component<{}, State> {
+  public state: State = {
     isApplicationArrived: false,
     isPaymentCompleted: false
   };
 
-  render() {
+  public render() {
+    const { isApplicationArrived, isPaymentCompleted } = this.state;
+
     return (
       <S.EditButtonWrapper>
         <S.CheckboxTitle>원서도착여부</S.CheckboxTitle>
@@ -25,7 +27,9 @@ class EditButtonContainer extends React.Component<any, State> {
           id="application-arrival-status"
         />
         <label htmlFor="application-arrival-status">
-          <S.ApplicationArrivalCheckBox />
+          <S.ApplicationArrivalCheckBox onClick={this.handleOnclickIsArrived}>
+            {isApplicationArrived && <img src={Checked} alt="check icon" />}
+          </S.ApplicationArrivalCheckBox>
         </label>
         <S.CheckboxTitle>결제여부</S.CheckboxTitle>
         <S.StatusCheckbox
@@ -34,12 +38,28 @@ class EditButtonContainer extends React.Component<any, State> {
           id="payment-status"
         />
         <label htmlFor="payment-status">
-          <S.PaymentCheckBox />
+          <S.PaymentCheckBox onClick={this.handleOnclickIsPaymentCompleted}>
+            {isPaymentCompleted && <img src={Checked} alt="check icon" />}
+          </S.PaymentCheckBox>
         </label>
-        <S.SubmissionCancelBtn>최종제출 취소</S.SubmissionCancelBtn>
+        <S.SubmissionCancelBtn onClick={this.handleOnclickCancelSubmit}>
+          최종제출 취소
+        </S.SubmissionCancelBtn>
       </S.EditButtonWrapper>
     );
   }
+
+  private handleOnclickIsArrived = () => {
+    this.setState({ isApplicationArrived: !this.state.isApplicationArrived });
+  };
+
+  private handleOnclickIsPaymentCompleted = () => {
+    this.setState({ isPaymentCompleted: !this.state.isPaymentCompleted });
+  };
+
+  private handleOnclickCancelSubmit = async () => {
+    await cancelSubmitApplicant({ email: "thisis@email.com" });
+  };
 }
 
 export default EditButtonContainer;
