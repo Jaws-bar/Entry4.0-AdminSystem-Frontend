5bcd1fc4ecd79a9ef5dea5e90b8fe23cbca8713c
sohi_5 <38098147+soheejang15@users.noreply.github.com>
2019-10-01 23:46:01 +0900

    MODIFY: Applicants list componentization

---
+++ haagen-dazs/src/components/ApplicantListContainer/style.ts
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const ApplicantListHeader = styled.thead`
+  height: 51px;
+  background-color: #ffffff;
+  box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.16);
+  opacity: 0.87;
+  font-size: 14px;
+  color: #222222;
+`;
+
+export const TR = styled.tr`
+  width: 100%;
+  height: 51px;
+  display: flex;
+  align-items: center;
+`;
+
+export const TH = styled.th`
+  flex: 1;
+`;
+
+export const TD = styled.td`
+  flex: 1;
+  display: flex;
+  justify-content: center;
+`;
